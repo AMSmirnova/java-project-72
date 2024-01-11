f789bd673b28a8e11cb292318940e50f5aedc63d
Anastasiia Smirnova <nyakubtsova@gmail.com>
2024-01-11 22:05:12 +0100

    a

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("java")
     id("jacoco")
     id("application")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 application {
@@ -16,6 +16,8 @@ repositories {
 }
 
 dependencies {
+    implementation("io.javalin:javalin:5.6.3")
+    implementation("org.slf4j:slf4j-simple:2.0.7")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,29 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+
+import java.util.Collections;
+
+public class App {
+
+    public static void main(String[] args) {
+        var app = getApp();
+        app.start(getPort());
+    }
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+
+        app.get("/", ctx -> ctx.result("Hello World"));
+
+        return app;
+    }
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.valueOf(port);
+    }
+}
