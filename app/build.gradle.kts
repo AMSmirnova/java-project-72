211cffc97cb73ce64c9c3db35d9e2f82ba734e67
Anastasiia Smirnova <nyakubtsova@gmail.com>
2024-01-15 12:57:54 +0100

    step 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,11 @@ repositories {
 }
 
 dependencies {
+    implementation("com.h2database:h2:2.2.220")
+    implementation("com.zaxxer:HikariCP:5.1.0")
     implementation("io.javalin:javalin:5.6.3")
     implementation("org.slf4j:slf4j-simple:2.0.7")
+    implementation("org.projectlombok:lombok:1.18.26")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,29 +1,69 @@
 package hexlet.code;
 
+import hexlet.code.repository.BaseRepository;
 import io.javalin.Javalin;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
-import java.util.Collections;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.sql.SQLException;
+import java.util.stream.Collectors;
+
 
 public class App {
 
-    public static void main(String[] args) {
+//    final String JDBC_DATABASE_DEFAULT = "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;";
+//    final String PORT_DEFAULT = "7070";
+
+    public static void main(String[] args) throws IOException, SQLException {
         var app = getApp();
         app.start(getPort());
     }
 
-    public static Javalin getApp() {
+    public static Javalin getApp() throws IOException, SQLException {
+
+        var hikariConfig = new HikariConfig();
+        var jdbcUrl = getJDBCUrl();
+
+        if (jdbcUrl.equals("jdbc:h2:mem:project")) {
+            hikariConfig.setJdbcUrl("jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
+        } else {
+            hikariConfig.setUsername(System.getenv("JDBC_DATABASE_USERNAME"));
+            hikariConfig.setPassword(System.getenv("JDBC_DATABASE_PASSWORD"));
+            hikariConfig.setJdbcUrl(jdbcUrl);
+        }
+
+        var dataSource = new HikariDataSource(hikariConfig);
+        var url = App.class.getClassLoader().getResource("schema.sql");
+        var file = new File(url.getFile());
+        var sql = Files.lines(file.toPath())
+                .collect(Collectors.joining("\n"));
+
+        try (var connection = dataSource.getConnection();
+                var statement = connection.createStatement()) {
+            statement.execute(sql);
+        }
+
+        BaseRepository.dataSource = dataSource;
 
         var app = Javalin.create(config -> {
             config.plugins.enableDevLogging();
         });
 
-        app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/", ctx -> ctx.result(jdbcUrl));
 
         return app;
+    }
+
+    private static String getJDBCUrl() {
+        return System.getenv().getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
     }
 
     private static int getPort() {
         String port = System.getenv().getOrDefault("PORT", "7070");
         return Integer.valueOf(port);
     }
+
 }
